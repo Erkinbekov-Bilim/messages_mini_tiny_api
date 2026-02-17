4952f66c80e2e0e24299f781f37434b2cf578dd2
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-17 18:23:37 +0600

    chore: moved PORT to constant and create PATH_MESSAGES

---
+++ src/constants/constants.ts
@@ -0,0 +1,2 @@
+export const PORT: number = 8000;
+export const MESSAGES_PATH: string = "src/messages";
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import messagesRouter from "./routes/messages.routes.js";
+import { PORT } from "./constants/constants.js";
 
 const app = express();
-const port = 8000;
 
 app.use(express.json());
 app.use("/messages", messagesRouter);
 
 const run = () => {
-  app.listen(port, () => {
-    console.log(`Server: ${port}`);
+  app.listen(PORT, () => {
+    console.log(`Server: ${PORT}`);
   });
 };
 
